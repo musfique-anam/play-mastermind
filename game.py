f229dd41bc52a96b5df85924326c2fefeb7c5ffe
Musfique Anam <musfique2002.personal@gmail.com>
2025-09-03 09:00:58 +0600

    Initial commit

---
+++ game.py
@@ -0,0 +1,41 @@
+import random
+import time
+
+print("Welcome to Mastermind (CLI Edition)")
+
+#Choose dificulty
+digits=int(input("Choose difficulty(3/4/5 digits):"))
+if digits < 3 or digits > 5:
+    digits = 4 #set as default
+
+#Generate Random Number
+num=random.randrange(10**(digits-1),10**digits)
+
+#Attempt limit
+max_attempts=10
+counter=0
+
+#Strat timer
+start=time.time()
+
+#First Guess
+n=int(input("Guess the {digits}-digit number: "))
+
+if n==num:
+    print("Great! You guessed the number in just 1 try! You're a mastermind!")
+else:
+    while n != num:
+        counter += 1
+
+        if counter>=max_attempts:
+            print("Game Over! You ran out of attempts.")
+            print("The number was:",num)
+            break
+
+        count=0
+        n=str(n)
+        num=str(num)
+        correct=['X']*digits
+
+        #Check digit by digit
+        
