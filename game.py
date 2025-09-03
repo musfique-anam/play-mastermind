e910e7d7893dac6cfd73d7c4a9699f907fdadbb2
Musfique Anam <musfique2002.personal@gmail.com>
2025-09-03 09:20:52 +0600

    final commit

--- game.py
+++ game.py
@@ -19,7 +19,7 @@ counter=0
 start=time.time()
 
 #First Guess
-n=int(input("Guess the {digits}-digit number: "))
+n=int(input(f"Guess the {digits}-digit number: "))
 
 if n==num:
     print("Great! You guessed the number in just 1 try! You're a mastermind!")
@@ -34,8 +34,31 @@ else:
 
         count=0
         n=str(n)
-        num=str(num)
+        num_str=str(num)
         correct=['X']*digits
 
         #Check digit by digit
-        
+        for i in range(digits):
+            if n[i] ==num_str[i]:
+                count += 1
+                correct[i] =n[i]
+
+
+        #Feedback
+        if(count < digits) and (count != 0):
+            print("Not quite the number. But you did get",count, "digit(s) correct!")
+            print("Also these numbers in your input were correct:")
+            print(' '.join(correct)) #add spaces between list
+            print("Attempts left:",max_attempts-counter)
+            n=int(input("Enter your next choice of numbers: "))
+
+        elif count == 0:
+            print("None of the numbers in your input match.")
+            print('Attempts left:',max_attempts-counter)
+            n=int(input("Enter your next choice of numbers: "))
+
+    if n == num:
+        end=time.time()
+        print("You've become a Mastermind!")
+        print("It took you only",counter, "tries")
+        print("Time taken", int(end-start), "seconds")
